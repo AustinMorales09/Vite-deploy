6b1a8c49e3d478e2dfa3cb8eb20691dabcd946b5
Austin <harrenaustint@gmail.com>
2025-03-12 09:43:28 -0400

    pushing backend url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://vite-backend-aqbv.onrender.com/'
+export default BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://vite-backend-aqbv.onrender.com/'
